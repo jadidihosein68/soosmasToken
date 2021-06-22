72e6441847408e6ac1f3b9d396845b5e637d7d05
Mohammad hossein jadidi <jadidi.hosein@gmail.com>
2021-06-22 22:59:25 +0800

    soosmas token

--- Token.sol
+++ Token.sol
@@ -1,11 +1,11 @@
-// contracts/GLDToken.sol
+// contracts/SOOSToken.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+contract SOSMSToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Soosmas", "SOSMS") {
         _mint(msg.sender, initialSupply);
     }
 }
